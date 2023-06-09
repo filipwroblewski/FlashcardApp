1134dcf2a9eb64885ec264507fb6117f854261fe
filipwroblewski <filip.wroblewski.row@gmail.com>
2023-06-09 13:22:57 +0200

    display all flashcards data

--- views/displayFlashcardsView.php
+++ views/displayFlashcardsView.php
@@ -6,18 +6,20 @@
 <body>
     <h1>Wszystkie fiszki</h1>
 
+    <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Powrót</a>
+
     <?php if (!empty($flashcards)): ?>
         <?php foreach ($flashcards as $flashcard): ?>
             <div class="flashcard">
                 <h3><?php echo $flashcard['name']; ?></h3>
                 <p>Kategoria: <?php echo $flashcard['category']; ?></p>
-                <!-- Other flashcard details -->
+                <p>Opis: <?php echo $flashcard['description']; ?></p>
+                <p>Czy ulubione: <?php echo $flashcard['favourite'] ? 'Tak' : 'Nie'; ?></p>
+                <p>Czy widziana: <?php echo $flashcard['seen'] ? 'Tak' : 'Nie'; ?></p>
             </div>
         <?php endforeach; ?>
     <?php else: ?>
         <p>Brak dostępnych fiszek.</p>
     <?php endif; ?>
-
-    <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Powrót</a>
 </body>
 </html>
