0bd2ce64fdf0397a80bd04b848e80964b0bc5a6b
filipwroblewski <filip.wroblewski.row@gmail.com>
2023-06-14 08:48:47 +0200

    add bootstrap and create design for adding flashcard view

--- views/addFlashcardView.php
+++ views/addFlashcardView.php
@@ -2,30 +2,40 @@
 <html>
 <head>
     <title>Dodawaj fiszkę</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Dodawanie nowej fiszki</h1>
+    <div class="container">
+        <h1 class="mt-4">Dodawanie nowej fiszki</h1>
 
-    <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>">Powrót</a>
+        <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" class="btn btn-primary mb-4">Powrót</a>
 
-    <form action="index.php?action=addFlashcard" method="POST">
-        <label for="category">Kategoria:</label>
-        <select name="category" id="category">
-            <?php
-                foreach ($categories as $category) {
-                    echo '<option value="' . $category['id'] . '">' . $category['name'] . '</option>';
-                }
-            ?>
-        </select>
+        <form action="index.php?action=addFlashcard" method="POST">
+            <div class="form-group">
+                <label for="category">Kategoria:</label>
+                <select name="category" id="category" class="form-control">
+                    <?php
+                        foreach ($categories as $category) {
+                            echo '<option value="' . $category['id'] . '">' . $category['name'] . '</option>';
+                        }
+                    ?>
+                </select>
+            </div>
 
-        <label for="name">Przód fiszki:</label>
-        <input type="text" name="name" id="name">
+            <div class="form-group">
+                <label for="name">Przód fiszki:</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
 
-        <label for="description">Tył fiszki:</label>
-        <textarea name="description" id="description"></textarea>
+            <div class="form-group">
+                <label for="description">Tył fiszki:</label>
+                <textarea name="description" id="description" class="form-control"></textarea>
+            </div>
 
-        <button type="submit">Dodaj fiszkę</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Dodaj fiszkę</button>
+        </form>
+    </div>
 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
